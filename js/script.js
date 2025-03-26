297fd05d96d730952a8a39bb2e0cc0f8b23cfa38
mohammadalisharifi <mohammadalisharifi@gmail.com>
2025-03-26 18:14:18 +0100

    Complete submission with all files

--- js/script.js
+++ js/script.js
@@ -533,41 +533,6 @@ class Timer {
   }
 }
 
-// Load all required JavaScript files
-const scriptFiles = [
-  "js/config.js",
-  "js/dom.js",
-  "js/state.js",
-  "js/storage.js",
-  "js/ui.js",
-  "js/api.js",
-  "js/events.js",
-  "js/timer.js",
-  "js/movieManager.js",
-];
-
-// Function to load scripts sequentially
-function loadScripts() {
-  let currentScript = 0;
-
-  function loadNextScript() {
-    if (currentScript < scriptFiles.length) {
-      const script = document.createElement("script");
-      script.src = scriptFiles[currentScript];
-      script.onload = () => {
-        currentScript++;
-        loadNextScript();
-      };
-      document.head.appendChild(script);
-    } else {
-      // All scripts loaded, initialize the application
-      initializeApp();
-    }
-  }
-
-  loadNextScript();
-}
-
 // Initialize Application
 function initializeApp() {
   uiComponents.createModal();
@@ -588,4 +553,4 @@ function initializeApp() {
 }
 
 // Start loading scripts when DOM is loaded
-document.addEventListener("DOMContentLoaded", loadScripts);
+document.addEventListener("DOMContentLoaded", initializeApp);
